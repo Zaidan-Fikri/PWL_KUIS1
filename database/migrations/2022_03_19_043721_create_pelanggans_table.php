be98490e6c46ddde30a001ec6f3bd9af12056c6d
Zaidan-Fikri <zaidanfikri11@gmail.com>
2022-03-22 16:06:20 +0700

    FINAL

--- database/migrations/2022_03_19_043721_create_pelanggans_table.php
+++ database/migrations/2022_03_19_043721_create_pelanggans_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('pelanggans', function (Blueprint $table) {
             $table->id();
-            $table->string ('nama',101);
-            $table->string ('jenis_kelamin',103);
-            $table->string ('no_hp',102);
+            $table->string ('nama',100);
+            $table->string ('jenis_kelamin',100);
+            $table->string ('no_hp',100); 
             $table->timestamps();
         });
     }
