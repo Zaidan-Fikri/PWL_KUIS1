baea7995b835677001e6819b916113580f0a6bba
= <=>
2022-03-22 22:30:24 +0700

    final

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -18,13 +18,23 @@ class SupplierSeeder extends Seeder
             [
                 [
                     'pemimpin' => 'Abdul',
-                    'nama_perusahaan' => 'Sejahtera',
+                    'nama_perusahaan' => 'PT.Sejahtera',
                     'no_perusahaan' => '0593'
                 ],
                 [
                     'pemimpin' => 'Abdul',
-                    'nama_perusahaan' => 'Sentosa',
-                    'no_perusahaan' => '0590'
+                    'nama_perusahaan' => 'PT.Sentosa',
+                    'no_perusahaan' => '0534'
+                ],
+                [
+                    'pemimpin' => 'Abdi',
+                    'nama_perusahaan' => 'PT.Makmur',
+                    'no_perusahaan' => '0546'
+                ],
+                [
+                    'pemimpin' => 'Bima',
+                    'nama_perusahaan' => 'PT.Jaya',
+                    'no_perusahaan' => '05756'
                 ]
                 
             ]
